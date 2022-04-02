a639c3bc8c3bd65fc9862cd8f209f16517e24edb
James <james.jenkins2@marist.edu>
2022-04-02 16:38:00 -0400

    Begin DSA Implementation
    Begin DSA implementation by creating necessary functions and data structures.

--- pkg/dsa/dsa.go
+++ pkg/dsa/dsa.go
@@ -2,8 +2,32 @@ package dsa
 
 import (
 	"fmt"
+	"math/big"
 )
 
 func main() {
 	fmt.Printf("dsa implementation\n")
+}
+
+type KeyPair struct {
+	P       big.Int
+	Q       big.Int
+	G       big.Int
+	Public  publicKey
+	Private privateKey
+}
+
+type privateKey big.Int
+type publicKey big.Int
+
+func GenerateKeyPair() KeyPair {
+	return KeyPair{}
+}
+
+func Sign() {
+
+}
+
+func Verify() {
+
 }
